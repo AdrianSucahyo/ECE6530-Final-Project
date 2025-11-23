25c0b3dc5de45bb80a53079d4b2ae05d814be9ff
williamHurst8 <dewaddictman@yahoo.com>
2025-11-22 19:23:53 -0700

    2.1, 2.2 mostly complete
    Bulk of 2.1 and 2.2 is complete, still has room for clean-up and clarification.
    
    -Added prompting user for launching GUI.
    -Moved script-defined resolution plotting to it's own function (plot_filter)
    -Changed phase plot from rad/s to deg

--- LabS-6_2LabExercise/main.m
+++ LabS-6_2LabExercise/main.m
@@ -1,16 +1,32 @@
 %% 2.1 
+clear; close all; clc;
 
-% Include DSP-First 'toolbox' to path
-addpath(genpath('filterdesign'));
+% Include DSP-First 'toolbox' to path if not already included
+if exist('filterdesign') == 0
+    addpath(genpath('filterdesign'));
+end
 
 % ** The DSP System Toolbox was installed for the filterdesign GUI.
 
+% Ask if running GUI is needed
+valid = 0;
+while valid == 0
+    runGUI = input('Would you like to run the filterdesign GUI? (Y/N)','s');
+    if (runGUI == 'y' || runGUI =='Y')
+        filterdesign; % Open the filter design GUI
+        valid = 1;
+    elseif runGUI == 'n' || runGUI == 'N'
+        valid = 1;
+    else
+        disp('Invalid input, type ''Y'' or ''N''')
+        valid = 0;
+    end
+end
 
-% filterdesign % Used this command to open the GUI
 % M = 30;
 % wc = 0.4*pi; % 2 * pi * (2000 / 10000)
 F_samp = 10000;
-samples = 10000; % Set the sample count for freqz
+samples = 100000; % Set the sample count for freqz
 
 % Rectangular windowing:
 % In the GUI:
@@ -22,29 +38,7 @@ samples = 10000; % Set the sample count for freqz
 % - Set "F_cutoff" to "2000"
 % - Export coefficients to a file (File → Export Coeffs → Export to File).
 
-% Import file
-coeffs = load("2_1_rect_coeffs.mat");
-
-% Extract variables
-rect_num_coeffs = coeffs.a;
-rect_den_coeffs = coeffs.b;
-
-[h,w] = freqz(rect_den_coeffs,rect_num_coeffs,'whole',samples);
-f = w*F_samp/(2*pi); % Convert rad/s to Hz by multiplying by F_samp from GUI and dividing by 2π
-
-% Rotate h and shift f to show lowpass behavior.
-h = circshift(h,(samples/2));
-f = f-(F_samp/2);
-
-figure
-subplot(2,1,1)
-plot(f,abs(h))
-xlabel('Frequency (Hz)')
-ylabel('Magnitude')
-subplot(2,1,2)
-plot(f,angle(h))
-xlabel('Frequency (Hz)')
-ylabel('Phase')
+plot_filter("2_1_rect_coeffs.mat",'Rectagular Windowed Filter',F_samp,samples)
 
 % Hamming windowing:
 % In the GUI:
@@ -56,35 +50,19 @@ ylabel('Phase')
 % - Set "F_cutoff" to "2000"
 % - Export coefficients to a file (File → Export Coeffs → Export to File).
 
-% Import file
-coeffs = load("2_1_hamm_coeffs.mat");
-
-% Extract variables
-hamm_num_coeffs = coeffs.a;
-hamm_den_coeffs = coeffs.b;
-
-[h,w] = freqz(hamm_den_coeffs,hamm_num_coeffs,'whole',samples);
-f = w*F_samp/(2*pi); % Convert rad/s to Hz by multiplying by F_samp from GUI and dividing by 2π
-
-% Rotate h and shift f to show lowpass behavior.
-h = circshift(h,(samples/2));
-f = f-(F_samp/2);
-
-figure
-subplot(2,1,1)
-plot(f,abs(h))
-xlabel('Frequency (Hz)')
-ylabel('Magnitude')
-subplot(2,1,2)
-plot(f,angle(h))
-xlabel('Frequency (Hz)')
-ylabel('Phase')
+plot_filter("2_1_hamm_coeffs.mat",'Hamming Windowed Filter',F_samp,samples)
 
 % Part a
+% 1851.2 Hz, |H| = 0.900047
+rect_p_edge = 1851.2;
 
 % Part b
+% 2148.3 Hz, |H| = 0.0998619
+rect_s_edge = 2148.3;
 
 % Part c
+% 1503.8 Hz, |H| = 0.989997
+hamm_p_edge = 1503.8;
 
 % --------------------------------------------------------------------------------
 % This isn't needed, it's being calculated in the GUI
@@ -92,18 +70,54 @@ ylabel('Phase')
 % --------------------------------------------------------------------------------
 
 % Part d
+% 2496.2 Hz, |H| = 0.100168
+hamm_s_edge = 2496.2;
 
 % Part e
+rect_cutoff = (rect_p_edge + rect_s_edge)/2;
+% Rectangular 1999.8
+hamm_cutoff = (hamm_p_edge + hamm_s_edge) / 2;
+% Hamming 2000
 
 %% 2.2
-
 % Part a
+rect_t_width = rect_s_edge - rect_p_edge;
+% Rectangular 297.1
+hamm_t_width = hamm_s_edge - hamm_p_edge;
+% Hamming 992.4
 
 % Part b
+% When comparing two Mth order filters, the one with a smaller transition width will havelarger ripples.
 
 % Part c
+% M = 60
+% wc = 0.4*pi; % 2 * pi * (2000 / 10000)
+
+% Hamming windowing with increased M:
+% In the GUI:
+% - Select FIR
+% - Set "Design Method" to "Window" and "Lowpass"
+% - Set "Window Type" to "Hamming"
+% - Set "Order" to "60"
+% - Set "F_samp" to "10000"
+% - Set "F_cutoff" to "2000"
+% - Export coefficients to a file (File → Export Coeffs → Export to File).
+
+plot_filter("2_2_hamm2_coeffs.mat",'Hamming Windowed Filter with Increased M',F_samp,samples);
+
+% Pb = 1751.9 Hz, 0.990007
+% Sb = 2248.1 Hz, 0.0100021
+hamm2_p_edge = 1751.9;
+hamm2_s_edge = 2248.1;
+hamm2_t_width = hamm2_s_edge - hamm2_p_edge;
+% Tb = 496.2
 
 % Part d
+% Tb = C / L
+% L = M + 1
+C1 = hamm_t_width * (30 + 1); % = 3.0764e+04
+C2 = hamm2_t_width * (60 + 1); % = 3.0268e+04
+% if we remove the '+1' in the above, both evaluate to 2.9772e+04
 
 %% 2.3
 
@@ -112,3 +126,43 @@ ylabel('Phase')
 % Part b
 
 % Part c
+
+
+%% Functions
+
+function plot_filter(coeff_file,plot_title,F_samp,samples)
+% This function takes the coefficient output from filterdesign and plots the magnitude and phase response using user-defined resolution.
+%   Inputs:
+%       coeff_file: A .mat file created by the filterdesign GUI that contains the numerator and denominator coefficients.
+%       plot_title: A string used to title the generated plot
+%       F_samp: The F_samp value used in the GUI to generate the GUI coefficients.
+%       samples: The sample count the user defines for the new plot.
+%
+%   Output:
+%       A single figure with magnitude and phase (degrees) plots plotted across frequency (Hz)
+
+    % Import file
+    coeffs = load(coeff_file);
+
+    % Extract variables
+    num_coeffs = coeffs.a;
+    den_coeffs = coeffs.b;
+
+    [h,w] = freqz(den_coeffs,num_coeffs,'whole',samples);
+    f = w * F_samp / (2 * pi); % Convert rad/s to Hz by multiplying by F_samp from GUI and dividing by 2π
+
+    % Rotate h and shift f to show lowpass behavior.
+    h = circshift(h,(samples / 2));
+    f = f - (F_samp / 2);
+
+    figure
+    subplot(2,1,1)
+    plot(f,abs(h))
+    xlabel('Frequency (Hz)')
+    ylabel('Magnitude')
+    subplot(2,1,2)
+    plot(f,rad2deg(angle(h)))
+    xlabel('Frequency (Hz)')
+    ylabel('Phase (degrees)')
+    sgtitle(plot_title)
+end
